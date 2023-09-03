808bc030fbcb3a0e86becc983a111bbbf9164066
twan <18330156177@163.com>
2023-09-03 14:38:43 +0800

    自定义窗口大小

---
+++ flink/src/main/scala/com/xiaofan/flink/ScalaStyleTest.scala
@@ -0,0 +1,26 @@
+package com.xiaofan.flink
+
+/** @description
+  *   ${description}
+  * @author
+  *   twan
+  * @date
+  *   2023-09-03 13:03:28
+  * @version 1.0
+  */
+object ScalaStyleTest {
+
+  def main(args: Array[String]): Unit = {}
+
+  def getMessage(message: String): Unit = {
+    val sql =
+      """
+        |select * from 
+        |student
+        |""".stripMargin
+    this.getClass.getClassLoader.getParent.getParent.getParent.getParent.getParent.getParent.getParent.getParent.getParent.getParent.getParent.getParent.getParent.getParent
+    println("atest")
+    if (true) println("test") else println("aaa")
+  }
+
+}
