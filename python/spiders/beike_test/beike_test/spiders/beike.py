104364ceb4307c3ecfaac47e8d7c52c02580fee1
wantao <wantao@inmyshow.com>
2023-09-15 10:16:50 +0800

    贝克落库问题处理

--- python/spiders/beike_test/beike_test/spiders/beike.py
+++ python/spiders/beike_test/beike_test/spiders/beike.py
@@ -1,7 +1,9 @@
+import re
+from datetime import datetime
+
 import scrapy
-from bs4 import BeautifulSoup
 from scrapy import Request
-import  re
+
 from spiders.beike_test.beike_test.items import BeikeTestItem
 
 
@@ -25,18 +27,25 @@ class BeikeSpider(scrapy.Spider):
     def parse_detail(self, response, **kwargs):
         item = kwargs['item']
         # 总价
-        item['total_price'] = response.xpath("//span[@class='total']/text()")
+        item['total_price'] = response.xpath("//span[@class='total']/text()").extract_first()
         # 2室1厅;北;61.85平米
-        main_infos = response.xpath("//div[@class='mainInfo']/text()")
+        main_infos = response.xpath("//div[@class='mainInfo']/text()").extract()
         item['room_info_str'] = main_infos[0]
         item['room_layout'] = main_infos[1]
         item['room_area'] = re.findall(r'\d+\.\d+', main_infos[2])
         # 贝壳id
-        item['id'] = re.findall(r'\d+', response.xpath("//div[@class='houseRecord']//span[@class='info']/text()")[0])
-        item['gua_pai_date'] =response.xpath("//div[@class='transaction']//li[1]/text()")
-        item['transaction_date'] =response.xpath("//div[@class='transaction']//li[3]/text()")
-        item['cell_name'] = response.xpath("//div[@class='communityName']//a[1]/text()")
-        item['area_name'] = response.xpath("//div[@class='areaName']//a[1]/text()")
-        item['street_name'] = response.xpath("//div[@class='areaName']//a[2]/text()")
-        item['room_age'] = response.xpath("//div[@class='transaction']//li[5]/text()")
+        item['id'] = \
+        re.findall(r'\d+', response.xpath("//div[@class='houseRecord']//span[@class='info']/text()").extract_first())[0]
+        gua_pai_date_str = response.xpath("//div[@class='transaction']//li[1]/text()").extract_first().replace(' ',
+                                                                                                               '').replace(
+            '\n', '')
+        item['gua_pai_date'] = datetime.strptime(gua_pai_date_str, '%Y年%m月%d日')
+        transaction_date_str = response.xpath("//div[@class='transaction']//li[3]/text()").extract_first().replace(' ',
+                                                                                                                   '').replace(
+            '\n', '')
+        item['transaction_date'] = datetime.strptime(transaction_date_str, '%Y年%m月%d日')
+        item['cell_name'] = response.xpath("//div[@class='communityName']//a[1]/text()").extract_first()
+        item['area_name'] = response.xpath("//div[@class='areaName']//a[1]/text()").extract_first()
+        item['street_name'] = response.xpath("//div[@class='areaName']//a[2]/text()").extract_first()
+        item['room_age'] = response.xpath("//div[@class='transaction']//li[5]/text()").extract_first()
         yield item
--- python/spiders/beike_test/ddl.sql
+++ python/spiders/beike_test/ddl.sql
@@ -2,13 +2,17 @@ CREATE TABLE second_house_info
 (
     id               bigint COMMENT '贝壳id',
     total_price      DECIMAL(10, 2) COMMENT '房屋总价万元',
-    room_info_str    VARCHAR(20) COMMENT '3室2厅',
-    room_layout      VARCHAR(20) COMMENT '南 北',
+    room_info_str    VARCHAR(20) COLLATE utf8mb4_unicode_ci COMMENT '3室2厅',
+    room_layout      VARCHAR(20) COLLATE utf8mb4_unicode_ci COMMENT '南 北',
     room_area        DECIMAL(20) COMMENT '面积',
-    gua_pai_date     VARCHAR(20) COMMENT '挂牌时间',
-    transaction_date VARCHAR(20) COMMENT '上次交易时间',
-    room_age         VARCHAR(20) COMMENT '房屋年限',
-    cell_name        VARCHAR(20) COMMENT '小区名称',
-    area_name        VARCHAR(20) COMMENT '区名',
-    street_name      VARCHAR(20) COMMENT '街道名称'
+    gua_pai_date     VARCHAR(20) COLLATE utf8mb4_unicode_ci COMMENT '挂牌时间',
+    transaction_date VARCHAR(20) COLLATE utf8mb4_unicode_ci COMMENT '上次交易时间',
+    room_age         VARCHAR(20) COLLATE utf8mb4_unicode_ci COMMENT '房屋年限',
+    cell_name        VARCHAR(20) COLLATE utf8mb4_unicode_ci COMMENT '小区名称',
+    area_name        VARCHAR(20) COLLATE utf8mb4_unicode_ci COMMENT '区名',
+    street_name      VARCHAR(20) COLLATE utf8mb4_unicode_ci COMMENT '街道名称',
+    href             varchar(2000) COMMENT '详情信息',
+    create_time      timestamp NULL default CURRENT_TIMESTAMP,
+    update_time      timestamp NULL default CURRENT_TIMESTAMP
+
 );
